4981b41342a74ddb37132ea30b65809b3e07784c
fast <fast@fast-Latitude-D520.(none)>
2014-05-04 17:30:35 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bismillah::Application.config.secret_key_base = '30b6bea7fcc2e5b41066f40f2e5dbda0a06c7b249e11951567d4e04950c020b7bed385f70946d682d9097833d67e3347a69bb72bbffc7d15a20ff9ac04e074d1'
